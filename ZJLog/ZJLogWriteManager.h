36b8b77af9abee37ca8dd3df37bb3db23152cadb
lzj <lizhijian_21@163.com>
2019-01-09 18:06:48 +0800

    1、临时提交；

---
+++ ZJLog/ZJLogWriteManager.h
@@ -0,0 +1,25 @@
+//
+//  ZJLogWriteManager.h
+//  ZJLog
+//
+//  Created by lzj<lizhijian_21@163.com> on 2019/1/8.
+//  Copyright © 2019 ZJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Singleton.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJLogWriteManager : NSObject
+singleton_h(Write);
+
+- (void)close;
+
+- (void)writeLog:(NSString *_Nullable)content;
+
+- (void)writeWrap;
+
+@end
+
+NS_ASSUME_NONNULL_END
