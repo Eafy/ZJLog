f9e07536c7d7a44bfafa52f2ce06362c4945882b
lzj <lizhijian_21@163.com>
2019-01-09 16:32:12 +0800

    临时提交

---
+++ ZJLogDemo/ZJLogDemo/TestLog.c
@@ -0,0 +1,9 @@
+//
+//  TestLog.c
+//  ZJLogDemo
+//
+//  Created by lzj<lizhijian_21@163.com> on 2019/1/9.
+//  Copyright © 2019 ZJ. All rights reserved.
+//
+
+#include "TestLog.h"
---
+++ ZJLogDemo/ZJLogDemo/TestLog.h
@@ -0,0 +1,14 @@
+//
+//  TestLog.h
+//  ZJLogDemo
+//
+//  Created by lzj<lizhijian_21@163.com> on 2019/1/9.
+//  Copyright © 2019 ZJ. All rights reserved.
+//
+
+#ifndef TestLog_h
+#define TestLog_h
+
+#include <stdio.h>
+
+#endif /* TestLog_h */
